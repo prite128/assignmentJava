9df19b18885552a4a049536e6480131c83455448
prite <varat.rat@hotmail.com>
2024-08-23 22:44:37 +0700

    init project

---
+++ src/main/java/com/varat/assignmentJava/AssignmentJavaApplication.java
@@ -0,0 +1,16 @@
+package com.varat.assignmentJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJavaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AssignmentJavaApplication.class, args);
+		System.out.println("test");
+
+	}
+
+}
---
+++ src/test/java/com/varat/assignmentJava/AssignmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varat.assignmentJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
