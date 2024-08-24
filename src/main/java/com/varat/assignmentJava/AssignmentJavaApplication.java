8bff00ace982204e20ba4a99b4a052e16fd2ca4a
prite <varat.rat@hotmail.com>
2024-08-24 21:16:29 +0700

    add solution and example question 1

--- src/main/java/com/varat/assignmentJava/AssignmentJavaApplication.java
+++ src/main/java/com/varat/assignmentJava/AssignmentJavaApplication.java
@@ -9,8 +9,35 @@ public class AssignmentJavaApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(AssignmentJavaApplication.class, args);
-		System.out.println("test");
+
+
+		// Q1
+		int[] Q1Ex1 = {7,1,5,3,6,4};
+		int[] Q1Ex2 = {7,6,4,3,1};
+		System.out.println(Q1(Q1Ex1));
+		System.out.println(Q1(Q1Ex2));
 
 	}
 
+	public static int Q1(int[] in){
+		int max = 0;
+		int left = 0;
+		int right = 0;
+
+		while( right < in.length ){
+
+			if(in[left]<in[right]){
+				int result = in[right]-in[left];
+				max = Math.max(max,result);
+			}
+			else{
+				left = right;
+			}
+			right++;
+		}
+
+		return max;
+	}
+
+	
 }
